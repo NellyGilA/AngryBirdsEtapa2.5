c5d4d4c3f5e47106260a7076cb347f8ceea8e24a
NellyGilA <ngil2387@gmail.com>
2022-01-26 09:41:19 -0600

    Agregamos platform

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
+  
+
+
+
+
+
+
+
+
+
+
+
 var box1, pig1;
 var backgroundImg;
 
@@ -17,6 +30,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170); 
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +73,5 @@ function draw(){
     log5.display();
 
     bird.display();
-}
+    platform.display();
+}   
